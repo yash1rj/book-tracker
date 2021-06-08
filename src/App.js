ac2210d0f35274f1bf91ef72c4d6e7545b2268d3
Yash Raj <yashrj130@gmail.com>
2021-06-08 23:19:19 +0530

    Create App.js
    Displayed the top level routed components

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route } from 'react-router';
+import './App.css';
+import Main from './components/Main/Main';
+import Search from './components/Search/Search';
+
+function App() {
+
+  return (
+    <div className="app">
+      <Route path="/" exact>
+        <Main />
+      </Route>
+      <Route path="/search" exact>
+        <Search />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
