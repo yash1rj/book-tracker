b7ecddf94a63899e32883fa7cb9ae7c2023765c0
Yash Raj <yashrj130@gmail.com>
2021-06-10 00:34:36 +0530

    Update App.js
    adding dropdownChanged handler to update shelf value of books in both main page and search page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import { Route } from 'react-router';
 import './App.css';
 import Main from './components/Main/Main';
 import Search from './components/Search/Search';
+import * as BooksAPI from './BooksAPI';
 
 function App() {
+
+  const dropDownChanged = (value, id) => {
+    BooksAPI.update({
+      id
+    }, value);
+  };
 
   return (
     <div className="app">
       <Route path="/" exact>
-        <Main />
+        <Main changer={dropDownChanged} />
       </Route>
       <Route path="/search" exact>
-        <Search />
+        <Search changer={dropDownChanged} />
       </Route>
     </div>
   );
