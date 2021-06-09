a67eb6b57aee100e39e530a626a2394b8e314884
Yash Raj <yashrj130@gmail.com>
2021-06-09 23:13:01 +0530

    Design of Bookshelves on Main page
    -render list of books in the defined shelf

---
+++ src/components/BookShelf/BookShelf.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Book from '../Book/Book';
+import './BookShelf.css';
+
+const BookShelf = props => {
+
+    const { bookData, id } = props;
+
+    let filteredBookData = bookData.filter(book => book.shelf === id);
+
+    let filteredBooks = filteredBookData.map(book => {
+        return <Book
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            authors={book.authors}
+            shelf={book.shelf}
+            imageLink={book.imageLinks || ""} />;
+    });
+
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{props.title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {filteredBooks}
+                </ol>
+            </div>
+        </div>
+    );
+};
+
+export default BookShelf;
