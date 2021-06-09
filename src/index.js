eb495148412f998a1bfc8ca85b3702b59f839fa5
Yash Raj <yashrj130@gmail.com>
2021-06-10 01:06:29 +0530

    Update index.js
    Wrapping with BooksContext Provider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { BooksContextProvider } from './store/books-context';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <BooksContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </BooksContextProvider>,
   document.getElementById('root')
 );
 
