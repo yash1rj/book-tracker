b8e6dd0022106c378c95d89998fc2d139d335195
Yash Raj <yashrj130@gmail.com>
2021-06-11 13:39:52 +0530

    Update Search.js
    autofocus on search input

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -83,7 +83,7 @@ const Search = props => {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                    <input type="text" placeholder="Search by title or author" onChange={searchHandler} />
+                    <input type="text" autoFocus placeholder="Search by title or author" onChange={searchHandler} />
 
                 </div>
             </div>
